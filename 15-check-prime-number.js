f12054cff547a9724dc47eebbdef1c5c820eb980
Newton <156936866+Newton2n@users.noreply.github.com>
2026-03-25 20:07:52 +0600

    Chore:Optimize version for checking prime number

--- 15-check-prime-number.js
+++ 15-check-prime-number.js
@@ -1,14 +1,13 @@
 function isPrimeNumber(number) {
   if (typeof number !== "number" || number < 1) {
-    throw new Error("Only number is allowed");
+    throw new Error("Only positive number is allowed");
   }
   if (number === 1) return false;
   if (number === 2) return true;
   if (number % 2 === 0) return false;
   let result = true;
 
-  for (let i = 3; i < number; i = i + 2) {
-    console.log(i);
+  for (let i = 3; i < number; i = i + 2) {;
     if (number % i === 0) {
       result = false;
       break;
@@ -26,4 +25,33 @@ console.log(isPrimeNumber(2)); //true
 console.log(isPrimeNumber(5)); //true
 console.log(isPrimeNumber(1)); //false
 console.log(isPrimeNumber(1259)); //true
-console.log(isPrimeNumber("122")); //Only number is allowed
+// console.log(isPrimeNumber("122")); //Only number is allowed
+
+function isPrimeNumber2(number) {
+  if (typeof number !== "number" || number < 1) {
+    throw new Error("Only positive number is allowed");
+  }
+  if (number === 1) return false;
+  if (number === 2) return true;
+  if (number % 2 === 0) return false;
+  let result = true;
+  const limit  = Math.sqrt(number)
+  for (let i = 3; i <= limit; i = i + 2) {
+    if (number % i === 0) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+}
+console.log(isPrimeNumber2(8)); //false
+console.log(isPrimeNumber2(81)); //false
+console.log(isPrimeNumber2(8)); //false
+console.log(isPrimeNumber2(9)); //false
+console.log(isPrimeNumber2(11)); //true
+console.log(isPrimeNumber2(3)); //true
+console.log(isPrimeNumber2(2)); //true
+console.log(isPrimeNumber2(5)); //true
+console.log(isPrimeNumber2(1)); //false
+console.log(isPrimeNumber2(1259)); //true
+console.log(isPrimeNumber2("122")); //Only number is allowed
